4546faaf9b28f38bf279b019121b448f1b6f6420
nanako0323 <08b39@gmail.com>
2020-05-31 10:37:34 +0900

    delete exe

--- music/main.go
+++ music/main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
-
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+// Dog ... dynamoDb struct
+type Dog struct {
+	Name string
+	Kind string
+}
 
 func main() {
 
-	//todo: credential file and region file
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
 	svc := dynamodb.New(sess)
-	input := &dynamodb.ListTablesInput{}
 
-	fmt.Printf("Tables:\n")
-
-	for {
-		result, err := svc.ListTables(input)
-
-		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case dynamodb.ErrCodeInternalServerError:
-					fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-				default:
-					fmt.Println(aerr.Error())
-				}
-			} else {
-				fmt.Println(err.Error())
-			}
-			return
-		}
-
-		for _, n := range result.TableNames {
-			fmt.Println(*n)
-		}
-
-		input.ExclusiveStartTableName = result.LastEvaluatedTableName
-
-		if result.LastEvaluatedTableName == nil {
-			break
-		}
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("Dog"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Name": {
+				S: aws.String("Ume"),
+			},
+			"Kind": {
+				S: aws.String("Mix"),
+			},
+		},
 	}
+
+	result, err := svc.GetItem(input)
+
+	if err != nil {
+		fmt.Println("GetItem Error", err)
+		return
+	}
+
+	fmt.Println(result)
+
+	dog := &Dog{}
+
+	//musicにmapし、エラーが発生した場合
+	if err := dynamodbattribute.UnmarshalMap(result.Item, dog); err != nil {
+		fmt.Println("Unmarshal Error", err)
+		return
+	}
+
+	j, _ := json.Marshal(dog)
+	fmt.Println(string(j))
 }
